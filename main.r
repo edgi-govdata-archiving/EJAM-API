c82ebae38c6cad0ad0b912412bb154b91ca4b29f
ejanalysis <ejam@ejanalysis.com>
2026-08-19 13:47:56 -0400

    Sync API version metadata and docs

--- main.r
+++ main.r
@@ -2,4 +2,16 @@
 library(plumber)
 
 r <- plumb("rest_controller.r")
+
+# Report the EJAM version used by this API in the Swagger/OpenAPI metadata.
+# The Docker image sets EJAM_VERSION; local runs fall back to the installed
+# package version.
+r <- plumber::pr_set_api_spec(r, function(spec) {
+  spec$info$version <- Sys.getenv(
+    "EJAM_VERSION",
+    unset = as.character(utils::packageVersion("EJAM"))
+  )
+  spec
+})
+
 r$run(port=8080, host="0.0.0.0")
